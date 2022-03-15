8d9f3f3f07e10f5a35b4a44817dc10bf1d5d0f46
Kim <wukimberly1@gmail.com>
2022-03-15 18:09:02 -0400

    tempate

--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import axios from "axios";
+import { connect, Provider } from "react-redux";
+import store, { loadUsers } from "./store";
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            users: [],
-            loading: true
-        };
-    }
-    async componentDidMount(){
-        this.setState({
-            users: (await axios.get('/api/users')).data,
-            loading: false,
-        });
-    }
-    render(){
-        const {users, loading} = this.state;
-        if(loading){
-            return '....loading';
-        }
-        return (
-            <ul>
-                {
-                    users.map(user => {
-                        return (<li key={user.id}> {user.name}</li>);
-                    })
-                }
-            </ul>
-        )
-    }   
+class _App extends Component {
+  // constructor() {
+  //     super();
+  //     this.state = {
+  //         users: [],
+  //         loading: true
+  //     };
+  // }
+  async componentDidMount() {
+    // this.setState({
+    //     users: (await axios.get('/api/users')).data,
+    //     loading: false,
+    // });
+    this.props.bootstrap();
+  }
+  render() {
+    return (
+      <div>
+        <h1> Acme Users </h1>
+        <ul>
+          {this.props.users.map((user) => {
+            return <li key={user.id}> {user.name} </li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
-render(<App />, document.querySelector('#root'));
+ const App = connect(
+     ({users}) => ({users}), (dispatch) => {
+     return {
+         bootstrap: () => dispatch(loadUsers())
+     }
+ }
+ )(_App);
+
+render(<Provider store={store}><App /></Provider>, document.querySelector("#root"));
